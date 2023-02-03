e906addb700d9ccd56459bd443f16e704a1c7041
ThejasriGolla <tejagolla95@gmail.com>
2023-02-03 15:03:10 -0500

    3

---
+++ src/Average.java
@@ -0,0 +1,10 @@
+//Write a Java method to compute the average of three numbers
+public class Average {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        int c=20;
+        int average =((a+b+c)/3);
+        System.out.println("Average of three numbers is "+average);
+    }
+}
