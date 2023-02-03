1f8ccf1ce38910aab7f6c6a9758c671d5dd89c96
ThejasriGolla <tejagolla95@gmail.com>
2023-02-03 14:51:13 -0500

    second commit
    second commit

---
+++ src/Smallest.java
@@ -0,0 +1,16 @@
+// Java program to find Smallest of three numbers
+public class Smallest {
+    public static void main(String[] args) {
+    int a=40;
+    int b=60;
+    int c=100;
+    if(a<=b && a<=c)
+        System.out.println(" a is smallest number "+a);
+    else if(b<=a && b<=c)
+        System.out.println(" b is smallest number "+b);
+    else {
+        System.out.println(" c is smallest number "+c);
+    }
+
+    }
+}
